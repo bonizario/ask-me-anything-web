1b50140e4d5d33cc7cf166b3280ab6d592209d66
Gabriel Bonizário <56419828+bonizario@users.noreply.github.com>
2024-08-11 00:33:09 -0300

    chore: add eslint and prettier configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
   languageOptions: {
@@ -18,9 +19,6 @@ export default tseslint.config({
   },
   rules: {
     ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-})
+});
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,3 @@
 export function App() {
-  return (
-    <h1>Ask Me Anything</h1>
-  );
+  return <h1>Ask Me Anything</h1>;
 }
